626cb39564320b921c9a489227d5629236060169
Akmal225d <islombahodirov424@gmail.com>
2024-11-01 17:00:44 +0500

    Add files via upload

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Wrapper.css'
+import Card from '../Card/Card'
+const Wrapper = () => {
+  return (
+    <>
+    <div className="wrapper">
+    <Card title="DOTA 2" text="5X5 turbo" num="1"/>
+    <Card title="DOTA 3" text="1x1 SOLO MID" num="2"/>
+        <Card title="CS:GO" text=" SOON" num="3"/>
+    </div>
+    </>
+  )
+  
+}
+
+export default Wrapper
